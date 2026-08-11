dc983d617bbad0eb5ac83fd111aca27ae7480324
Shubham Singh <141335242+shubham4734singh@users.noreply.github.com>
2026-08-12 01:20:13 +0530

    Fix Swagger UI layout and presets

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,11 +25,17 @@ app.get('/api/docs', (req: Request, res: Response) => {
     <body>
       <div id="swagger-ui"></div>
       <script src="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js" crossorigin></script>
+      <script src="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-standalone-preset.js" crossorigin></script>
       <script>
         window.onload = () => {
           window.ui = SwaggerUIBundle({
             url: '/api/swagger.json',
             dom_id: '#swagger-ui',
+            presets: [
+              SwaggerUIBundle.presets.apis,
+              SwaggerUIStandalonePreset
+            ],
+            layout: "StandaloneLayout"
           });
         };
       </script>
